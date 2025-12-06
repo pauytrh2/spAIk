6f512cd9e5ce71a018eba6472f7ace4b0685e133
pauytrh2 <pauytrh@gmail.com>
2025-12-06 13:10:22 +0200

    Add pyodide

---
+++ main.js
@@ -0,0 +1,27 @@
+let pyodide;
+pyodide = await loadPyodide();
+
+const toolbox = document.getElementById('toolbox');
+const workspace = Blockly.inject('blocklyDiv', {
+    toolbox: toolbox
+});
+
+async function runCode() {
+    const code = Blockly.Python.workspaceToCode(workspace);
+    console.log("Generated code:\n" + code);
+
+    if (!pyodide) {
+        alert("Pyodide not loaded yet!");
+        return;
+    }
+
+    try {
+        let result = await pyodide.runPythonAsync(code);
+        console.log("Result:", result);
+    } catch (err) {
+        console.error(err);
+        alert("Error: " + err);
+    }
+}
+
+window.runCode = runCode;
