4a9d305b83b32384cb28cdae30f645be4b53c43d
pauytrh2 <pauytrh@gmail.com>
2025-12-09 19:28:05 +0200

    Add stage api

--- main.js
+++ main.js
@@ -1,14 +1,66 @@
 let pyodide;
 pyodide = await loadPyodide();
+pyodide.registerJsModule("game", {
+    createSprite,
+    moveSprite,
+    setSpritePosition,
+    setSpriteColor,
+});
 
 const toolbox = document.getElementById('toolbox');
 const workspace = Blockly.inject('blocklyDiv', {
     toolbox: toolbox
 });
 
-async function runCode() {
-    const code = Blockly.Python.workspaceToCode(workspace);
-    console.log("Generated code:\n" + code);
+const stage = document.getElementById("stage");
+const sprites = {};
+
+function createSprite(name, x = 0, y = 0, color = "red") {
+    if (sprites[name]) return sprites[name];
+
+    const el = document.createElement("div");
+    el.className = "character";
+    el.style.left = x + "px";
+    el.style.top = y + "px";
+    el.style.backgroundColor = color;
+    stage.appendChild(el);
+
+    sprites[name] = el;
+    return el;
+}
+
+function moveSprite(name, dx, dy) {
+    const s = sprites[name];
+    if (!s) return;
+
+    const x = parseInt(s.style.left) + dx;
+    const y = parseInt(s.style.top) + dy;
+    s.style.left = x + "px";
+    s.style.top = y + "px";
+}
+
+function setSpritePosition(name, x, y) {
+    const s = sprites[name];
+    if (!s) return;
+    s.style.left = x + "px";
+    s.style.top = y + "px";
+}
+
+function setSpriteColor(name, color) {
+    const s = sprites[name];
+    if (!s) return;
+    s.style.backgroundColor = color;
+}
+
+async function runCode(mancode) {
+    let code;
+    if (!mancode) {
+        code = Blockly.Python.workspaceToCode(workspace);
+        console.log("Generated code:\n" + code);
+    } else {
+        code = mancode;
+        console.log("Manual code:\n" + code);
+    }
 
     if (!pyodide) {
         alert("Pyodide not loaded yet!");
